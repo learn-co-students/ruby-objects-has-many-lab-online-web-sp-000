8396d54ac6120735d540f864218294737ea2449b
Jonathan Mines <minesja@gmail.com>
2018-05-17 14:19:30 -0400

    Made changes to readme and tests. Tests and readme no longer expect the has-many to hold an array of the instances it has many of. Students should be writing methods that go through the collection of intances that belongs-to the has-many to select only the instances that actually belong to it.

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -3,31 +3,27 @@ require 'pry'
 
 describe "Author" do
 
-  let!(:betty) { Author.new("Betty") } 
+  let!(:betty) { Author.new("Betty") }
 
-    describe "#new" do 
-      it "is initialized with a name" do 
+    describe "#new" do
+      it "is initialized with a name" do
         expect{Author.new("Betty")}.to_not raise_error
-      end
-
-      it "is initialized with an empty collection of posts" do 
-        expect(betty.instance_variable_get(:@posts)).to match([])
       end
     end
 
     describe "#name" do
-      it "has an attr_accessor for name" do 
+      it "has an attr_accessor for name" do
         expect(betty.name).to eq("Betty")
     end
 
-    describe "#posts" do 
+    describe "#posts" do
       it "has many posts" do
         expect(betty.posts).to be_a(Array)
       end
     end
 
-    describe "#add_post" do 
-      it "takes in an argument of a post and adds that post to the author's collection and tells the post that it belongs to that author" do 
+    describe "#add_post" do
+      it "takes in an argument of a post and associates that post with the author by telling the post that it belongs to that author" do  
         hello_world = Post.new("Hello World")
         betty.add_post(hello_world)
         expect(betty.posts).to include(hello_world)
@@ -35,8 +31,8 @@ describe "Author" do
       end
     end
 
-    describe "#add_post_by_title" do 
-      it "takes in an argument of a post title, creates a new post with it and associates the post and author" do 
+    describe "#add_post_by_title" do
+      it "takes in an argument of a post title, creates a new post with it and associates the post and author" do
         betty.add_post_by_title("My Great Blog Post")
         # binding.pry
         expect(betty.posts.last.title).to eq("My Great Blog Post")
@@ -44,13 +40,13 @@ describe "Author" do
       end
     end
 
-    describe ".post_count" do 
-      it "is a class method that returns the total number of posts associated to all existing authors" do 
+    describe ".post_count" do
+      it "is a class method that returns the total number of posts associated to all existing authors" do
         # binding.pry
         expect(Author.post_count).to eq(2)
       end
 
-      it "uses the class variable, @@post_count" do 
+      it "uses the class variable, @@post_count" do
         expect(Author.class_variable_get(:@@post_count)).to be_a(Integer)
       end
     end
