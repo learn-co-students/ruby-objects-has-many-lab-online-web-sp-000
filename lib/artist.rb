20482f9a4b6e39972e04ce60b93c4ba7080e5fbd
Jonathan Mines <minesja@gmail.com>
2018-05-17 14:23:22 -0400

    first

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,7 @@ class Artist
   end
 
   def songs
-    @songs 
+    @songs
   end
 
   def self.song_count
--- lib/author.rb
+++ lib/author.rb
@@ -22,7 +22,7 @@ class Author
   end
 
   def posts
-    @posts 
+    @posts
   end
 
   def self.post_count
