f980dab375e48694cbdfc6fa2d96b7aa9001b021
maxwellbenton <maxwellbenton@gmail.com>
2018-09-10 11:24:10 -0400

    rebase from master, updated solution

---
+++ lib/author.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+class Author
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+end
