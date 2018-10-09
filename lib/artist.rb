0c5e0be9ab9d4c53b731d49893194843b20fb3aa
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-09 17:40:45 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist
+  attr_accessor :name, :songs
+  
+  @@song_count = 0
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+  
+  def add_song_by_name(name)
+    new_song = Song.new(name)
+    new_song.artist = self
+    @songs << new_song
+    @@song_count += 1
+  end
+  
+  def self.song_count
+    @@song_count
+  end
+end
