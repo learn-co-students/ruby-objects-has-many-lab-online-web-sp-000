d57954b8a6aaec7c9aae8773b2515043b2063470
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-31 20:39:14 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+class Post
+  attr_accessor :author, :title
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    self.author.name
+  end
+
+end
