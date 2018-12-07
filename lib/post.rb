492437fc0b2acf96247c86f8dcf5b1ad9d1de57f
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-07 20:15:21 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+
+  attr_accessor :title, :author
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def author_name
+    self.author ? self.author.name : nil
+  end
+
+  def self.all
+    @@all
+  end
+
+end
