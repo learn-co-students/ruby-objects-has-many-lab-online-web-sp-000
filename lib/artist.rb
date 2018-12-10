db24f81699b8e7d32ce46de72cd2bcbf374ca51b
Meisuleen Kased <meisuleen@gmail.com>
2018-12-10 04:35:39 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,7 @@
+require 'pry'
+
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
@@ -11,9 +13,19 @@ class Artist
   end
 
   def add_song(song)
-    song = Song.new(song)
     @songs << song
     song.artist = self
   end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+
 
 end
