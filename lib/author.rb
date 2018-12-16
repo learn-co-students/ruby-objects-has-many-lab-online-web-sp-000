473732c3c5af9a1b22a7872c091905f191558bd5
Milena Kari <milenakari@yahoo.com>
2018-12-16 19:32:13 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+  attr_accessor :name
+  @@all_posts = []
+  def initialize(name)
+    @name=name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    post.author = self
+    @posts << post
+    @@all_posts << post
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author=self
+    @posts << post
+    @@all_posts << post
+  end
+
+  def self.post_count
+    @@all_posts.size
+  end
+end
