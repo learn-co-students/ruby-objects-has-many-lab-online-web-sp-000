80cd68e7fde7754d05d1533e2740216ab1c642db
Kyle  Beckstead <kyle21beckstead@gmail.com>
2018-12-27 23:28:43 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,27 @@
+class Author 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_title(post)
+    post = Post.new(post)
+    post.author = self
+  end
+  
+  def posts 
+    Post.all 
+  end
+  
+  def self.post_count
+    Post.all.count
+  end
+  
+  
+  
+end
