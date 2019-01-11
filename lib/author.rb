f331c2f943c053fb4ce81076193a947fedc2b3d1
Anup Khadka <akhadka.dev@gmail.com>
2019-01-11 20:13:50 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,26 @@
+class Author
+
+  attr_accessor :name, :posts
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    post.author = self
+    @posts << post
+    @@post_count += 1
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
+end
