f8427c05889cf1afe7e1e368a216bc44ca9e9b89
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-02-27 20:44:07 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,33 @@
 class Author
+attr_accessor :name, :post
+@@all = []
+
+def initialize (name)
+  @name = name
+@posts = []
+end
+
+def posts
+@posts
+end
+
+def add_post(post)
+  @posts << post
+  @@all << post
+  post.author = self
+end
+
+def add_post_by_title(title)
+  post = Post.new(title)
+  @posts << post
+  @@all << post
+  post.author = self
+end
+
+def self.post_count
+@@all.size
+end
+
+
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,22 @@
 class Song
-attr_accessor :artist
+attr_accessor :artist, :name 
+@@all = []
 
 def initialize (name)
 @name = name
+@@all << self
+end
+
+def self.all
+@@all
+end
+
+def artist_name
+  if self.artist == nil
+    nil
+  else
+    self.artist.name
+  end
 end
 
 end
