4bcead2e79a6708038dd63e1d2d1942459211b1c
Jonathan Wong <jonathan.wong110@gmail.com>
2019-03-08 04:02:24 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+  attr_accessor :name
+  
+  @@post_count = 0
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def posts
+    @posts
+  end
+  
+  def add_post(post)
+    post.author = self
+    self.posts << post
+    @@post_count += 1
+  end
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.add_post(post)
+  end
+  
+  def self.post_count
+    @@post_count
+  end
+end
