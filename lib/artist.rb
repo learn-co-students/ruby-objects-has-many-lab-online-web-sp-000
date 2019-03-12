fdd6c7852ccbf1ff507fcfc4ad20a384d3f24f93
ADIL NAIMI <adilnm23@hotmail.com>
2019-03-12 10:14:56 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+class Artist
+  attr_accessor :name
+  @@count=0
+  def initialize(name)
+    @name=name
+    @songs=[]
+    
+    
+  end
+def add_song(song)
+    @songs << song
+    song.artist = self
+    @@count+=1
+
+  end
+  
+  def songs
+    @songs
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+      song.artist = self
+      @@count+=1
+    
+  end
+  
+  def self.song_count
+    @@count
+  end
+
+end
