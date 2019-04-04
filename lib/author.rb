bc3a15570b64c50c510e563222efb4f86848f2cd
Karen V <karenvo@usc.edu>
2019-04-04 06:54:45 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,9 @@
 class Author
   attr_accessor :name, :posts
-  @@post_count = 0
+  @@post_count = 0 # counter outside of initialize
   def initialize(name)
     @name = name
-    @posts = []
+    @posts = [] # empty array post
   end
   def author_name # if there is no author, return nil
     if self.author
@@ -18,9 +18,9 @@ class Author
     post.author = self
   end
   def add_post_by_title(title)
-    post = Post.new(title)
-    @posts << post
-    post.author = self
+    post = Post.new(title) # passing on a title into new post
+    @posts << post # adds to the array of posts
+    post.author = self # tells you who wrote the post, self (class)
     @@post_count += 1
   end
   def posts
