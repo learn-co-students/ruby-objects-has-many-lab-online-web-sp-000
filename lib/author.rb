39c228edb6c6fc1453c2ec5818d54f130942445c
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-15 15:49:03 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,32 @@
+class Author
+  attr_accessor :name
+
+  @@posts = []
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @@posts << post
+    @posts << post
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @@posts << post
+    @posts << post
+    post.author = self
+  end
+
+  def self.post_count
+    @@posts.length
+  end
+
+end
