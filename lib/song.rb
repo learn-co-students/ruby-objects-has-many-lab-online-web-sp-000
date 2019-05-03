0578a6d467878107b45da0abf907cbaae2aef92a
Emilia Ziemichód <emifrompoland@gmail.com>
2019-05-03 23:08:10 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+
+  attr_accessor :name, :artist
+  @@all=[]
+
+  def initialize(name)
+    @name = name
+    @@all<<self
+  end
+
+  def artist_name
+    if @artist == nil
+      return nil
+    else
+      @artist.name
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+end
