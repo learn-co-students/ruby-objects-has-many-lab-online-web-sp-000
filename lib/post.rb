a787647d81af6b438a71f50773215e7773c027cf
Andrew Silva <andrewsilva9@gmail.com>
2019-05-12 22:46:14 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,9 @@
+class Post
+  attr_accessor :title
+  @@all = []
+
+  def initialize(title)
+    @title = title
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+end
