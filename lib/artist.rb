72047497dd89131e8e4367b901f9ad8957107039
Andrew Silva <andrewsilva9@gmail.com>
2019-05-13 03:36:29 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,7 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
+
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -7,11 +9,27 @@ class Artist
   end
 
   def add_song(song)
-    @songs << song
+    self.songs << song
+    song.artist = self
+    @@song_count += 1
   end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
 
   def songs
     @songs
   end
+
+  def self.song_count
+    @@song_count
+  end
+
+
 
 end
--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,23 @@
 class Post
-  attr_accessor :title
+  attr_accessor :title, :author
   @@all = []
 
   def initialize(title)
     @title = title
+    @author = author
+    @@all << self
+  end
+
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
+  end
+
+  def self.all
+    @@all
   end
 
 end
