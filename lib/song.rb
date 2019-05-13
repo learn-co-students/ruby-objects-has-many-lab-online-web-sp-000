5f21487d90f9eaa00d628fc3edbf3b3a3e228434
Paul Rosero <paul_rosero@yahoo.com>
2019-05-13 23:42:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,19 @@
 class Song
-  attr_accessor :artist, :name
-   
-  def initialize(name)
-     @name = name
+  attr_accessor :name, :artist 
   
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    artist.name if artist
   end
   
 end
