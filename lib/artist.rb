6c66c81c6ebf5a7dfeb90b80f1479be030f19fb7
Alan Krajina <alankrajina88@gmail.com>
2019-05-26 14:18:31 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,37 +1,31 @@
-require "pry"
-
 class Artist
-  attr_accessor :name, :artist
-  
-  @@song_count = 0
+  attr_accessor :name
 
-  
+  @@all = []
+
   def initialize(name)
     @name = name
-    @songs = []
+    @@all << self
   end
-  
-  def songs
-    @songs
+
+  def self.all
+    @@all
   end
-  
+
   def add_song(song)
-    @songs << song
     song.artist = self
-    @@song_count += 1
   end
-  
+
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
     song.artist = self
-    @@song_count += 1
   end
-  
-  def self.song_count
-    @@song_count
-  end
-  
-end
 
-# Artist .song_count is a class method that returns the total number of songs associated to all existing artists
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+end
