b90e14c5108184e945a39389626b9af97a0dc13f
maharvey2 <mah0013mobile@gmail.com>
2019-06-09 09:28:31 -0500

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,18 @@
 class Post
     attr_accessor :title, :author
-    
+  
     @@all = []
-    
+  
     def initialize(title)
-        @title = title
-        @@all << self
+      @title = title
+      @@all << self
     end
-end
+  
+    def self.all
+      @@all
+    end
+  
+    def author_name
+      author.name if author
+    end
+  end
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,18 @@
 class Song
     attr_accessor :name, :artist
-    
+  
     @@all = []
-    
+  
     def initialize(name)
-        @name = name
-        @@all << self
+      @name = name
+      @@all << self
     end
-end
+  
+    def self.all
+      @@all
+    end
+  
+    def artist_name
+      artist.name if artist
+    end
+  end
