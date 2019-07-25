ecf79ec34f2d9e0454f7a803a1a03d89c0229c4b
Anthony Molina <antonio-molina@att.net>
2019-07-25 22:25:50 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,27 @@
+class Post
+  
+  attr_accessor :title, :author
+    @@all = []
+  def initialize(title) 
+    @title = title
+    @@all << self
+  end 
+  
+  def author_name 
+    if @author == nil 
+      return nil 
+    else 
+      @author.name 
+    end 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+end 
+
+  
+  
+  
+  
+  
