318c32c6ea44a1b16cda1f1915b43c7c29af93f1
Garrett Thompson <gtcruz.gt@gmail.com>
2019-08-14 02:54:27 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist
+  attr_accessor :name
+
+  @@song_count = 1
+
+  def initialize(name)
+    @name = name
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def add_song(song)
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+end
---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+
+  attr_accessor :name
+
+  @@post_count = 1
+
+  def initialize(name)
+    @name = name
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def add_post(post)
+    post.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
+  end
+end
