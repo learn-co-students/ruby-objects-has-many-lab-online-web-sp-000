ae674bd8572dbdd31a82b5532b39ebb363fb34dd
Zach  Smith <zdsmith001@gmail.com>
2019-08-23 16:32:47 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,12 @@
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_song(song)
+
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+class Song
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+end
