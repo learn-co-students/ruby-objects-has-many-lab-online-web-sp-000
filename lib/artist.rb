a198278828f4aa1fc9a0d64557e360ec079ce177
Raul Sposito <raulsposito@gmail.com>
2019-09-03 14:18:00 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+  attr_accessor :name
+
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+    @@song_count +=1
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    @@song_count
+  end
+  
+end
