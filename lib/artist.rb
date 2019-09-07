63f12a03620ab1d611a04ccb9bb8cb770700797d
Michael Kofron <kofronmichael@gmail.com>
2019-09-07 05:20:56 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist
+  attr_accessor :name, :songs
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  @@song_count = 1
+  
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+  
+  def songs
+    @songs 
+  end
+  
+  def self.song_count
+    @@song_count
+  end
+  
+end
