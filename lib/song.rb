2e7686d0bbc9695af8ab93ce6f2ee30bc3e090b0
Kimberly Rouse <ksrouse94@gmail.com>
2019-09-19 01:41:59 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song 
+  attr_accessor :artist, :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artist_name
+    self.artist.name if self.artist
+  end
+  
+end  
