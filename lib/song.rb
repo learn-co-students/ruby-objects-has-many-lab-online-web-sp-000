d71ce46cb29f1368b7a03afe598a4167caee59c0
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-01 17:30:46 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+
+ attr_accessor :name, :artist
+
+ @@all = []
+
+ def initialize(name)
+   @name = name
+   @@all << self
+ end
+
+ def self.all
+   @@all
+ end
+
+ def artist_name
+   if self.artist != nil
+     self.artist.name
+   end
+ end
+
+end
