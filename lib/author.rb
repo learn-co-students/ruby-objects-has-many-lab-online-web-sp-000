720c9da4f220456da5bfa3be4a90fa4fe64d0096
Blythe Pollard <blytheepollard@gmail.com>
2019-10-10 00:11:58 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,1 +1,26 @@
 #has many posts
+class Author
+  attr_accessor :name, :post, :title
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def add_post(post)
+    post.author = self
+  end
+  
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+  
+  def add_post_by_title(title)
+    title = Post.new(title)
+    title.author = self
+  end
+  
+  def self.post_count
+    Post.all.count
+  end
+end
--- lib/post.rb
+++ lib/post.rb
@@ -3,3 +3,21 @@
 #need to have :author
 ##individual author will need a method that returns collection of posts that belong to author--will need to have class variable holding all instances of every post in an array
 #class method that returns class variable holdingthose instances
+
+class Post
+  attr_accessor :author, :title
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+  
+ def self.all
+    @@all
+  end
+  
+  def author_name
+    author.name if author
+  end
+end
