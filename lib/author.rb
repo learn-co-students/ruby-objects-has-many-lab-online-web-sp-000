59d3cdc06e6695af4a888d7c5ae63b3f201fa9ec
Anthony Tilelli <anthony@tilelli.me>
2019-10-23 00:21:59 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,2 +1,6 @@
 class Author
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
 end
--- lib/post.rb
+++ lib/post.rb
@@ -1,2 +1,14 @@
 class Post
+ attr_accessor :title, :author
+  @@all = []
+  
+  def initialize(title)
+    @title = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
