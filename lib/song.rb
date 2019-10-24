aa6dac1909097ea24f3de12565e81573cc3cb7c4
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-24 01:55:17 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song
+  attr_accessor :artist, :name
+    @@all = []
+
+  def initialize(name)
+   @name = name
+   save
+  end
+
+  def name
+    @name
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    self.artist.name if artist
+  end
+end
