ebfd06f8727985ee5893528f9f1148ad80ccd59b
Jazmin M <jazminmerckel@gmail.com>
2019-10-25 02:55:19 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,28 @@
+class Post 
+  
+  attr_accessor :author, :title
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    save
+  end
+ 
+  def save
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def author_name
+    if @author == nil
+      return nil
+    else
+      return @author.name
+    end
+  end
+  
+end
