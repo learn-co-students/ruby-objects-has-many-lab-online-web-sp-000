2f7e4e26f894a704b9673837ba6d49c65582f42b
Nicholas Ng <nicholasykng@gmail.com>
2019-10-29 02:59:12 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+  @@all = []
+  attr_accessor :title, :author
+  def initialize(title)
+    @title = title
+    @author = author
+    @@all << self
+  end
+
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
+  end
+  def self.all
+    @@all
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+  attr_accessor :artist, :name
+  @@all = []
+  def initialize(name, artist=nil)
+    @name = name
+    @artist = artist
+    @@all << self
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+  end
+end
+
+def self.all
+  @@all
+end
+
+end
