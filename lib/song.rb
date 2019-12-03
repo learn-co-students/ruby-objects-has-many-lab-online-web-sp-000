2b4fc18f3b9a33d58a098f6c1909b33405c0646b
wei chen <weichen9083@gmail.com>
2019-12-03 02:55:40 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+    class Song
+      attr_accessor :artist, :name
+     
+      @@all = []
+     
+      def initialize(name)
+        @name = name
+
+        save
+      end
+     
+      def save
+        @@all << self
+      end
+     
+      def self.all
+        @@all
+      end
+      
+      def artist_name
+        if self.artist == NIL
+        else self.artist.name
+        end 
+      end 
+    end
