dd62ed60155f311087e7728a5f1d727a5b5a71ba
Nicole Rothenay <nthorn1211@gmail.com>
2019-12-31 23:33:23 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,27 @@
+class Post
+  attr_accessor :title, :author
+  
+  @@all = []
+  
+  def initialize(title, author=nil)
+    @title = title
+    @author = author
+    @@all << self
+  end
+  
+  def all
+    
+  end
+
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
+  end
+  
+  def self.all
+    @@all
+  end
+end
