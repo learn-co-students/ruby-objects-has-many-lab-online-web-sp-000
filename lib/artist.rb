fac6b138ce021747b6393b296692cd653a6140fc
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-08 03:22:40 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist
+
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_song_by_name(song)
+    new_song = Song.new(song)
+    add_song(new_song)
+  end
+
+  def self.song_count
+    artist.song.count
+  end
+
+end
