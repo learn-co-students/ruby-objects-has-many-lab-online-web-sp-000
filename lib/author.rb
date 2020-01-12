395c2bfeb9820f1587f3ed4d76bbb5424618ccad
Sherief Farid <sherief@farid.cloud>
2020-01-12 02:20:10 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,27 @@
+require 'pry'
+
 class Author
-  attr_accessor :name, :post
-  # def initialize(name)
-  #   @name = name
-  #   @posts = []
-  # end
-  # def add_post(title,author = self)
-  #   new_post = {}
-  #   new_post = Post.new(title,author)
-  #   @post << new_post
-  # end
+  attr_accessor :name, :posts
+  @@posts_count = 0
+
+  def initialize (name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @@posts_count += 1
+    post.author = self
+    post.author.posts << post
+  end
+
+  def add_post_by_title(post_name) #where is name???
+    s = Post.new(post_name)
+    self.add_post(s)
+  end
+
+  def self.post_count
+    @@posts_count
+  end
+
 end
--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,29 @@
+#rspec ./spec/song_spec.rb --f-f
+
 class Post
-  #@@all = []
-  attr_accessor :title, :author
-  # def initialize(title,author)
-  #   @title = title
-  #   @author = author
-  #   @@all << self
-  # end
-  # def self.all
-  #   @@all
-  # end
-  # def author(author)
-  #   @author = author
-  #   author.posts << self
-  # end
+  attr_accessor :title, :author, :name
+  @@all = []
+    def initialize(title)
+      @title = title
+      @name = title
+      @@all << self
+    end
+
+    def author=(author)
+      @author = author
+      author.posts << self
+    end
+
+    def author_name
+      if !self.author
+        return nil
+      else
+        self.author.name
+      end
+    end
+
+    def self.all
+      @@all
+    end
+
 end
