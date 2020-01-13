74bf5255dabff61498581a411d5c55bc8517342f
Will Carter <will.carter@fergusllc.com>
2020-01-13 00:16:28 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+  attr_accessor :author, :title
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    if(self.author)
+      self.author.name
+    end
+  end
+
+end
