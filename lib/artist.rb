644da34f06bfd3a15d1aa365b6d7492df6ab1c45
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-23 22:55:17 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def songs
+    Song.all.select {|s| s.artist == self}
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song)
+  end
+
+  def self.song_count
+    Song.all.length
+  end
+end
---
+++ lib/author.rb
@@ -0,0 +1,24 @@
+class Author
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def posts
+    Post.all.select {|p| p.author == self}
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    add_post(post)
+  end
+
+  def self.post_count
+    Post.all.length
+  end
+end
