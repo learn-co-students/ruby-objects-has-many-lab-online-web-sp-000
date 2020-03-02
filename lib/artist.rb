7a280e5785060ff8a4c0ad8ae8c0ba6e99da040b
Mark Drehmann <markdrehmann@gmail.com>
2020-03-02 17:36:09 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+
+end
