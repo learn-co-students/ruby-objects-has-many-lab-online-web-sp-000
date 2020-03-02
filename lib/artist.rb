27580f883de4c15d0adee66ea205422a245b198e
Jake Garcia <bballwiz4@gmail.com>
2020-03-02 17:58:06 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
+
+  def self.song_count
+    Song.all.size
+  end
+
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+class Post
+  @@all = []
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+      if @author == nil
+        return nil
+      else
+        return @author.name
+      end
+  end
+
+end
