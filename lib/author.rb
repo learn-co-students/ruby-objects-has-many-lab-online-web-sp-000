f7d77e829069daa48f894e7bfc5309462df16ab5
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-17 15:07:29 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,28 @@
 class Author 
-  attr_accessor :posts
+  attr_accessor :posts, :name, :title
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+    
+  def posts 
+    Post.all.select {|posts| posts.author == self}
+  end
+  
+  def add_post(post_title)
+    @posts << post_title
+    author.name = self if author.name != self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    Post.author = self
+  end
+    
+  def self.post_count 
+    Post.all.length 
+  end
+
 end
--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,32 @@
 class Post 
-  attr_accessor :author
+  attr_accessor :author, :title
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title 
+    save
+  end
+  
+   def save
+    @@all << self
+   end
+  
+  def self.all
+    @@all 
+  end
+  
+  def author_name
+    if @author == nil 
+      return nil 
+    else 
+      @author.name 
+    end 
+  end 
+     
+  def author=(author)
+    @author = author
+  end
+  
+  
 end
