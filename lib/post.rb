07001e57196c1b98909db624f0ee0f4750655260
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-02 18:42:49 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,27 @@
+class Post
+
+  @@all = []
+
+  attr_accessor :title, :author
+  def initialize(title)
+    @title = title
+    save
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
