fa90a646bf01f0d1a462c7bfa0e0b9c30d864ffe
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-25 01:48:19 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+require 'pry'
+class Artist 
+  
+  attr_accessor :name, :song
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end 
+
+  def add_song(song)
+    song.artist = self
+    # binding.pry
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    Song.all.count 
+  end
+  
+  
+end 
