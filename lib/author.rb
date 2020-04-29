5ae72fa557551ad3ec5636b280b5ebf0e8d50898
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-04-29 21:46:42 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,25 @@
+class Author
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def posts
+    Post.all
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def self.post_count
+    Post.all.collect {|post| post.title}.count
+  end
+
+end
