174e9b3d33405ee6c4adcb2c9075a7d083c32dae
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-05-08 02:54:42 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,11 @@
+class Artist
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+ def songs
+   @songs << Song.new(name)
+ end
+end
---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  attr_accessor :artist, :name
+ 
+  @@all= []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+    def artist_name
+    if @artist == nil
+      return nil
+    else
+      @artist.name
+    end
+  end
+end
