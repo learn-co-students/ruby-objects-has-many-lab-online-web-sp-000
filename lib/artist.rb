2f8d07dd4f2192cacbeec4b1328df6e4660e0484
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-05-09 03:04:05 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,26 @@
+require 'pry'
+
 class Artist
   attr_accessor :name
+  @@song_count = 0
   
   def initialize(name)
     @name = name
-    @songs = []
   end
  def songs
-   @songs << Song.name.artist
+   Song.all.select {|song| song.artist == self}
+ end
+ def add_song(song)
+   song.artist = self
+   @@song_count+=1
+ end
+ def add_song_by_name(song_name)
+ song_name = Song.new(song_name)
+ add_song(song_name)
+ @@song_count +=1
+ end
+ 
+ def self.song_count
+   @@song_count
  end
 end
--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,23 @@
 class Author
   attr_accessor :name
+  @@post_count = 0
   def initialize(name)
     @name = name
     @posts= []
   end
+   def posts
+   Post.all.select {|post| post.author == self}
+ end
+ def add_post(post)
+   post.author = self
+   @@post_count +=1
+ end
+ def add_post_by_title(post_title)
+ post_title = Post.new(post_title)
+ add_post(post_title)
+ @@post_count+=1
+ end
+ def self.post_count
+   @@post_count
+ end
 end
