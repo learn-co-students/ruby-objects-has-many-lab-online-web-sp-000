55f1722828492baf5508976071e989c8b908f215
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-31 15:07:14 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+  attr_accessor :artist, :name
+
+  @@all = []
+
+ def initialize(name)
+   @name = name
+   save
+ end
+
+ def save
+   @@all << self
+ end
+
+ def self.all
+   @@all
+ end
+
+ def artist_name
+   self.artist.name unless self.artist.nil?
+end
+end
