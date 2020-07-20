d7af31782c4700c2e43f45a47fb6118c8eef63d5
Martin Moy <martinmoy1991@gmail.com>
2020-07-20 18:29:11 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,24 +1,19 @@
-
-
 class Artist
+
   attr_accessor :name
-
-
-
 
   def initialize(name)
     @name = name
   end
 
-  def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
-  end
-
   def add_song(song)
     song.artist = self
+  end
 
+  def songs
+    Song.all.select {
+      |song| song.artist
+    }
   end
 
   def add_song_by_name(name)
@@ -30,6 +25,5 @@ class Artist
     Song.all.count
 
   end
-
 
 end
