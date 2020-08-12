8de7de6639422a8b481b09427fbbc9f2678a2a89
Leslie G <lesliegonzalez@me.com>
2020-08-12 00:55:20 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  attr_accessor :name, :artist
+  
+  @@all = []
+ 
+  def initialize(name, artist = nil)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def artist_name
+    artist.name if artist
+  end
+
+end
