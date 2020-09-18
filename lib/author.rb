9192129086550d9a611c2de34361ba31f14a6969
Claire Bullock <bullockcm13@gmail.com>
2020-09-18 18:15:19 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,34 @@
+require 'pry'
+class Author
+
+  attr_accessor :name, :posts
+
+  @@post_count = 0
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts 
+      Post.all.select do |post|
+      post.author == self 
+      @posts
+    end
+  end
+  
+  def add_post(post)
+    post.author = self 
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+end
