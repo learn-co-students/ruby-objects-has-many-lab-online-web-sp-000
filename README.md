d76353b50a3330399d27d5c7286cf9136d1663c2
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-22 17:22:23 +0000

    Done.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Ruby Objects Has Many Lab
 
 ## Objectives
---
+++ lib/song.rb
@@ -0,0 +1,41 @@
+class Song
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artist_name
+    self.artist.name if self.artist
+  end
+end
+
+
+
+=begin
+class Song
+  attr_accessor :title, :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artist_name
+    self.artist.name if self.artist
+  end
+end
+=end
