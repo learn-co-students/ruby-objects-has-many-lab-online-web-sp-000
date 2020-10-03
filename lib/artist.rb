80fdc983d1ddf9e64fce55925bd680c2fc011415
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-03 22:18:35 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,44 +1,17 @@
+require 'pry'
 class Artist
+  
   attr_accessor :name
   
   def initialize(name)
     @name = name
   end
   
-  def add_song(song)
-    song.artist = self
-  end
-  
   def songs
-    Song.all.select {|song| song.artist == self}
-  end
-  
-  def add_song_by_name(name)
-    song = Song.new(name)
-    add_song(song)
-  end
-  
-  def self.song_count
-    Song.all.length
-  end
-end
-
-
-=begin
-class Artist
-  attr_accessor :name
-  
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-  
-  def songs
-    Song.all.select {|song| song.artist == self}
+    Song.all.select{|s| s.artist == self}
   end
   
   def add_song(song)
-    @songs << song
     song.artist = self
   end
   
@@ -48,7 +21,6 @@ class Artist
   end
   
   def self.song_count
-    Song.all.collect {|song| song.artist == self}.count
+    Song.all.count
   end
 end
-=end
