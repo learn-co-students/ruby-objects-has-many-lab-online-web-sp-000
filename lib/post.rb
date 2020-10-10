85fa2fa66fcfb9a732df026bcb34aff8050b2e90
Alan Tang <giabao_tang@yahoo.com>
2020-10-10 00:47:05 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,16 @@
+class Post 
+  attr_accessor :title, :author
+  @@all = []
+  def initialize(title)
+    @title = title 
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def author_name 
+    self.author.name if self.author
+  end
+end
