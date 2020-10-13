fbd068c3734da283e3b830ab0a77fbc394d55bd7
Christerle Nounoute <christerlesn@gmail.com>
2020-10-13 03:13:19 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,25 @@
+class Author
+attr_accessor :name
+
+def initialize(name)
+  @name = name
+  @post = []
+end
+
+def posts
+  Post.all
+end
+
+def add_post(post)
+post.author = self
+end
+
+def add_post_by_title(title)
+  post = Post.new(title)
+  add_post(post)
+end
+
+def self.post_count
+  Post.all.count
+end
+end #end of class Author
