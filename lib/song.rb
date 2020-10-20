56bab0eccc5f8adbbf4fcbf39808a6454c0505c8
mccray austin <tubach75@hotmail.com>
2020-10-20 14:37:45 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+
+  def initialize(name = "Survivor")
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+end
