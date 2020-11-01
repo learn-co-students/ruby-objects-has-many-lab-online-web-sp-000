8d81f7f5af4a671cee1a5921e9af753018f548f2
Damian Malysza <damian.malysza@gmail.com>
2020-11-01 19:21:37 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def songs
+    @songs = Song.all.select {|song| song.artist == self}
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(song)
+    Song.new(song).artist = self
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+
+end
---
+++ lib/author.rb
@@ -0,0 +1,24 @@
+class Author
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(post)
+    Post.new(post).author = self
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+
+end
