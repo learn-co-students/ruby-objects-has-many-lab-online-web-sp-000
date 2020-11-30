37d9b18fa168cdd10f6dcd3b5eb1684d46e17b11
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-30 04:21:46 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,9 +2,16 @@ require 'pry'
 
 class Artist
   attr_accessor :name
-
+  
+  @@all = []
+  
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
   
   def songs
--- lib/author.rb
+++ lib/author.rb
@@ -3,21 +3,28 @@ require 'pry'
 class Author
   attr_accessor :name
   
+  @@all = []
+  
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
   
   def posts
-    Post.all.select {|title| title.author == self}
+    Post.all.select {|post| post.author == self}
   end
   
-  def add_post(title)
-    title.author = self
+  def add_post(post)
+    post.author = self
   end
   
   def add_post_by_title(title)
-    title = Post.new(title)
-    title.author = self
+    post = Post.new(title)
+    post.author = self
   end
   
   def self.post_count
