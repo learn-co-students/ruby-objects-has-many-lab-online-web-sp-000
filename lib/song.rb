902d789f3e191bba7de8cd3870c91759a8c1f686
Luis Martinez <luis_mmartinez@live.com>
2021-01-04 04:22:37 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+require "pry"
+class Song
+
+  attr_accessor :name, :artist
+  @@all = [ ]
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    end
+  end
+
+end
