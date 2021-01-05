de5e421a0c7d299ea3d566772ca8e6dffb86f50a
Shaina C <scailles23@gmail.com>
2021-01-05 06:29:32 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    @@song_count += 1
+    @@song_count
+  end
+  
+
+end
