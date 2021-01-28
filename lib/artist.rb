3d2527cc4c3e01cdcaf36bdb2bc957d40b93b1b6
Shami Tomita <shamitomita@gmail.com>
2021-01-28 15:52:03 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+  attr_accessor :name, :genre 
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def add_song(song)
+    song.artist = self
+  end
+ 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
+  
+  def self.song_count
+   count = 0 
+   Song.all.select do |song| 
+     song.artist == self 
+     count += 1
+   end 
+   count
+ end 
+  
+  
+end
---
+++ lib/author.rb
@@ -0,0 +1,32 @@
+class Author 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def posts
+    Post.all.select {|post| post.author == self}
+  end 
+  
+  def add_post(post)
+    post.author = self
+  end 
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end 
+  
+  def self.post_count
+    count = 0
+    Post.all.select do |post|
+      post.author == self 
+      count+=1
+    end 
+    count 
+  end 
+
+  
+  
+end 
