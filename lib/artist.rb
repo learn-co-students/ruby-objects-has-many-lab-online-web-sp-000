cb1a13eeedda246291be5d91de8598223d17d174
HELEN HARMAN <hlharman@bellsouth.net>
2021-02-02 04:04:45 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,4 +8,15 @@ class Artist
   def add_song(song)
     song.artist = self 
   end 
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end 
+  
+  def add_song_by_name(name) 
+    song = Song.new(name)
+    add_song(song)
+  end 
+  
+  
 end 
