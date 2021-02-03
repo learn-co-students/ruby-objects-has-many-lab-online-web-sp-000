25a16931f8b3f1c63e66a85afcac2a6599c71db2
HELEN HARMAN <hlharman@bellsouth.net>
2021-02-03 19:42:51 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,16 @@
 class Artist 
-  attr_accessor :name 
+  attr_accessor :name, :artist 
   
   def initialize(name)
     @name = name 
   end 
   
   def add_song(song)
-    Song.artist = self 
+    song.artist = self
   end 
   
   def songs 
-    Song.all.select {|song| song.artist == self}
+    Song.all.select {|song| song.artist == self} 
   end 
   
   def add_song_by_name(name) 
--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   end 
   
   def artist_name
-    self.artist.name 
+    self.artist.name
   end 
   
   def save
