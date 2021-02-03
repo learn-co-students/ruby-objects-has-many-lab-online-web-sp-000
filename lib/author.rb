a7af67527caec6598edef4e48f9442a847f2b3da
HELEN HARMAN <hlharman@bellsouth.net>
2021-02-03 20:38:04 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,25 +1,27 @@
 class Author 
- attr_accessor :name 
+ attr_accessor :name, :post  
   
   
-  def initialize(name = nil)
+  def initialize(name)
     @name = name 
   end 
   
-  def add_posts(posts)
+  def add_post(posts)
    posts.author = self 
   end 
- 
-  def posts 
-    Posts.all.select {|post| post.Author == self}
+  
+ def posts 
+    Posts.all.select {|post| posts.author == self} 
+    # posts = Post.new(title)
+    # add_post(posts) 
   end 
   
   def add_posts_by_title(title)
     posts = Post.new(title)
-    add_posts(posts) 
+    add_post(posts) 
   end 
   
-   def self.posts_count 
+   def self.post_count 
     count = 0 
     Post.all.select {|post| count += 1}
     count 
