ae159566cf9c2cca69bd77d39e68a876a18ec28b
HELEN HARMAN <hlharman@bellsouth.net>
2021-02-04 23:46:55 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -24,31 +24,3 @@ end
     @@all
   end 
 end 
-
-
-
-# Hi, I'm still having trouble with the author_name method and the song_name method and could use some help. 
-
-# 1) Post #author_name returns nil if the post does not have an author
-#     Failure/Error: self.author.name
-
-#     NoMethodError:
-#       undefined method `name' for nil:NilClass
-#     # ./lib/post.rb:15:in `author_name'
-#     # ./spec/post_spec.rb:55:in `block (3 levels) in <top (required)>'
-
-#   2) Song #artist_name returns nil if the song does not have an artist
-#     Failure/Error: self.artist.name
-
-#     NoMethodError:
-#       undefined method `name' for nil:NilClass
-#     # ./lib/song.rb:11:in `artist_name'
-#     # ./spec/song_spec.rb:55:in `block (3 levels) in <top (required)>'
-
-
-
-
-
-# sure thing. I would start with self.author
-# HELEN HARMAN 14 MINUTES AGO
-# #author => nil self.author.name => "Sophie" I can't use name because the only time this is mentioned is the attr_accessor
