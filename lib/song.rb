db60b3a0d15292ef55cc270f04b2b5e58c5b68d8
Mike Nanto <mike@thecowboymechanic.com>
2021-02-12 07:06:56 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song 
+  attr_accessor :name, :artist  
+  
+  @@all = []
+  
+  def self.all 
+    @@all
+  end 
+  
+  def initialize(name)
+  @name = name
+  @@all << self 
+  end 
+  
+  def artist_name
+    artist.name if artist 
+  end 
+
+end 
