243e9711482b75b779d2bb857fee0b388e40667d
Matt Wolford <matthew.r.wolford@gmail.com>
2021-03-15 01:34:33 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,25 @@
+class Author
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def posts
+    Post.all.select { |post| post.author == self}
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    x = Post.new(title)
+    x.author = self
+  end
+
+  def self.post_count
+    Post.all.size
+  end
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,27 @@
+class Post
+  attr_accessor :title, :author
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    if self.author == nil
+      nil
+    else
+      self.author.name
+    end
+  end
+
+end
