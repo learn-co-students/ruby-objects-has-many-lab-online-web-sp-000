9532edf637a98d0d1824c4c845281082c07a91c2
Daysha Jones <daysha.vu@icloud.com>
2021-03-17 19:39:25 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
+  
+  def self.song_count
+    Song.all.count
+  end
+  
+end
