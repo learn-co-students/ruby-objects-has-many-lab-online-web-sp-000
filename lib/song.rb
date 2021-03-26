7f8daff4198fa7f996e680ce30c4d2f2271b8399
Cody Alvarez <cody_alvarez19@outlook.com>
2021-03-26 15:54:34 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  attr_accessor :artist, :name, :genre
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @genre = genre
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
