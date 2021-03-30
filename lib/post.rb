9ed9c9852a335e8e83db109aefa8f736b5440a82
Evelyn Kosma <ezranaja@gmail.com>
2021-03-30 23:40:02 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post
+  
+  attr_accessor :author, :title 
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def author_name
+    if author 
+      self.author.name 
+    else
+      nil
+    end
+  end
+  
+   def self.all
+    @@all
+  end
+end
