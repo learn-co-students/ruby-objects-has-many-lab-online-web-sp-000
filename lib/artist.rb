338c2afb6999113f0a68a86d2a1a7f32827b1ff7
Erica Little <eteney90@gmail.com>
2021-04-07 21:55:53 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select {|a| a.artist == self}
+  end
+
+  def add_song_by_name(song)
+    song = Song.new(song)
+    song.artist = self
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+
+end
