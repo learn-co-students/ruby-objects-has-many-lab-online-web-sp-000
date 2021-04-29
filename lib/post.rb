b5d38195554e9f498e67a1531d14cc452cbbc818
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-04-29 00:14:38 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class Post 
+  attr_accessor :title, :author 
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def author_name 
+    if !self.author
+      nil 
+    else 
+      self.author.name
+    end
+  end
+end 
