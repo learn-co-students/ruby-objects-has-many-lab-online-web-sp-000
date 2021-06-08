b0e26f63431dfea2e20572a38dba4fa41e45cd60
DJ Taylor <dtaylor88@gmail.com>
2021-06-08 23:10:19 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+  attr_accessor :name, :artist   
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  
+  def self.all
+    @@all 
+  end 
+  
+  def artist_name 
+    if artist 
+      artist.name 
+    end 
+  end 
+ 
+end 
