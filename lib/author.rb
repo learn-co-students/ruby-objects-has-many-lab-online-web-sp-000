8f768560af77e6439e1d60b897f0b6d091afb7cd
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-06-10 20:44:00 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,14 +1,9 @@
 class Author
   attr_accessor :name
-  @@post_count = 0
 
   def initialize(name)
     @name = name
     @posts = []
-  end
-
-  def posts
-    @posts
   end
 
   def posts
@@ -18,21 +13,16 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
-    @@post_count += 1
   end
 
   def add_post_by_title(post_title)
-    post = Post.new(post_title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-    p = Post.new(name)
-    self.add_post(p)
+    p = Post.new(post_title)
+    add_post(p)
     p
   end
 
   def self.post_count
-    @@post_count
+    Post.all.size
   end
 
 end
